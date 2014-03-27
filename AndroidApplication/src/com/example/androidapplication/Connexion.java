08fa69defc229b7706acc698f3a6faadbe96d4e1
bezeid Moulayeely <ob005696@etu.unice.fr>
2014-03-27 20:22:08 +0100

    ajout du projet de devellopement

---
+++ AndroidApplication/src/com/example/androidapplication/Connexion.java
@@ -0,0 +1,18 @@
+package com.example.androidapplication;
+
+/**
+ * Created by yezide on 26/03/14.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Connexion extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.connexion);
+    }
+}
+
---
+++ AndroidApplication/src/com/example/androidapplication/Inscription.java
@@ -0,0 +1,17 @@
+package com.example.androidapplication;
+
+/**
+ * Created by yezide on 26/03/14.
+ */
+
+import android.os.Bundle;
+
+public class Inscription extends MainActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      //  setContentView(R.layout.inscription);
+    }
+
+}
